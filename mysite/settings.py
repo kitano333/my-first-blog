83ab6b88f36ec228c5945f2177c8c13b302581c7
kitano333 <poao333@yahoo.co.jp>
2019-10-09 00:12:19 +0900

    Modified templates to display posts from database.and setting

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,6 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '04#*y3oi6az01mra)=g5cu3munn*t&udd+^&_glect+xbr!%hs'
 
 # SECURITY WARNING: don't run with debug turned on in production!
+#DEBUG = True
 DEBUG = False
 # web環境で実行する場合は、ここをfalseにすること（情報漏洩を防ぐため）。DEBUG = True
 
@@ -119,5 +120,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATIC_URL = '/static/'
+STATIC_URL = 'http://poaotest123.pythonanywhere.com/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, './static')
